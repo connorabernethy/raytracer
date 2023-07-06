1d0dc9a0213d1fe67a4337aaaa76005bc45809e0
connorabernethy <cjaberne@asu.edu>
2023-07-06 15:26:53 -0700

    Updated to add shading using normal vectors and an abstract hittable class to implement hittable objects.

--- Raytracer/main.cpp
+++ Raytracer/main.cpp
@@ -5,13 +5,19 @@
 
 // We can simulate a ray hitting a sphere at the origin of the scene via the following function:
 
-bool hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3& center, double radius, const ray& r) {
     vec3 oc = r.origin() - center;
-    auto a = dot(r.direction(), r.direction());
-    auto b = 2.0 * dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius * radius;
-    auto discriminant = b * b - 4 * a * c;
-    return (discriminant > 0);
+    auto a = r.direction().length_squared();
+    auto half_b = dot(oc, r.direction());
+    auto c = oc.length_squared() - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+
+    if (discriminant < 0) {
+        return -1.0;
+    }
+    else {
+        return (-half_b - sqrt(discriminant)) / a;
+    }
 }
 
 
@@ -19,11 +25,14 @@ bool hit_sphere(const point3& center, double radius, const ray& r) {
 // Linear interpolation!
 
 color ray_color(const ray& r) {
-    if (hit_sphere(point3(0, 0, -1), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t = hit_sphere(point3(0, 0, -1), 0.5, r);
+    if (t > 0.0) {
+        vec3 normal = unit_vector(r.at(t) - vec3(0, 0, -1));
+        return 0.5 * color(normal.x() + 1, normal.y() + 1, normal.z() + 1);
     }
+
     vec3 unit_direciton = unit_vector(r.direction());
-    auto t = 0.5 * (unit_direciton.y() + 1.0);
+    t = 0.5 * (unit_direciton.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
 }
 
